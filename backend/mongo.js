5f9acea4fc0d16765bd051c94a3558ac7d954d4b
Rameshguestene B. Mbonya-Bate <kingsdomain101@gmail.com>
2026-06-30 18:35:32 -0500

    Day 1 project setuo and database connections

---
+++ backend/mongo.js
@@ -0,0 +1,14 @@
+const { MongoClient } = require("mongoDb");
+let db;
+async function connectMongo() {
+    const client = new MongoClient(process.env.MONGO_URI);
+    await client.connect();
+    db = client.db("ecommerce_db");
+    console.log("MongoDB connected");
+}
+function getMongo() {
+    return db;
+}
+module.exports = {
+    connectMongo, getMongo
+};
